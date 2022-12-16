fba674232135ce27d07c8ef1ef04d83a74e0184f
HovhannesArustamyan <hovhannes.arust@gmail.com>
2022-12-16 18:20:52 +0400

    requirements.txt file added to the project

--- from_bgr_to_hsv.py
+++ from_bgr_to_hsv.py
@@ -1,9 +1,9 @@
 import cv2 as cv
 
 img = cv.imread('images/adele.jpg')
-img = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+img_ = cv.cvtColor(img, cv.COLOR_BGR2HSV)
 r, g, b = cv.split(img)
 
-img_ = cv.merge([b, g])
-cv.imshow("Result", img_)
-cv.waitKey(3000)
+img__ = cv.merge([b, g])
+cv.imshow("Result", img__)
+cv.waitKey(6000)
--- objects.py
+++ objects.py
@@ -10,5 +10,5 @@ img_or = cv.bitwise_or(circle, square)
 img_xor = cv.bitwise_xor(circle, square)
 img_not = cv.bitwise_not(square)
 
-cv.imshow("Result", img)
+cv.imshow("Result", img_not)
 cv.waitKey(4000)
